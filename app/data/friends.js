cea45fce7ed39b112cf9b2dc7b3981ae213989cf
Danielle <danineubauerr@gmail.com>
2020-02-25 14:34:22 -0800

    fixes bugs

--- app/data/friends.js
+++ app/data/friends.js
@@ -26,30 +26,3 @@ const friends = [
 
 module.exports = friends;
   
-
-// let matchMaker = () => {
-//   let lastFriend = friends.slice(-1)
-//   const calcOne = [];
-//   let addition = 0;
-//   const calcTwo = [];
-//   for (i = 0; i < friends.length - 1; i++) {
-//     for (t = 0; t < friends[i].scores.length; t++) {
-//       calcOne.push(Math.abs(parseInt(friends[i].scores[t]) - parseInt(lastFriend[0].scores[t])));
-//     };
-//     for (x = 0; x < calcOne.length; x++) {
-//       addition += calcOne[x];
-//     };
-//     calcTwo.push(addition);
-//     calcOne.length = 0;
-//     addition = 0;
-//   };
-//   let minIndex = calcTwo.indexOf(Math.min(...calcTwo));
-//   const match = [{ name: friends[minIndex].name, photo: friends[minIndex].photo }];
-//   calcTwo.length = 0;
-//   return match;
-// };
-
-// module.exports = {
-//   friends,
-//   matchMaker
-// };
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -9,26 +9,7 @@ module.exports = function (app) {
   });
 
   app.get('/api/friends/match', function (req, res) {
-    console.log('calculating match')
-    let lastFriend = friends.slice(-1)
-    const calcOne = [];
-    let addition = 0;
-    const calcTwo = [];
-    for (i = 0; i < friends.length - 1; i++) {
-        for (t = 0; t < friends[i].scores.length; t++) {
-            calcOne.push(Math.abs(parseInt(friends[i].scores[t]) - parseInt(lastFriend[0].scores[t])));
-        };
-        for (x = 0; x < calcOne.length; x++) {
-            addition += calcOne[x];
-        };
-        calcTwo.push(addition);
-        calcOne.length = 0;
-        addition = 0;
-    };
-    let minIndex = calcTwo.indexOf(Math.min(...calcTwo));
-    const match = [{ name: friends[minIndex].name, photo: friends[minIndex].photo }];
-    calcTwo.length = 0;
-    res.json(match);
+    res.json(friends);
   })
 
   app.post('/api/friends', function (req, res) {
