3d16f49c3181a07c44d01ebfd699c9c1b7322251
Danielle <danineubauerr@gmail.com>
2020-02-25 10:29:54 -0800

    fixes bugs

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,36 +5,51 @@ const friends = [
     name: 'Audrey Hepburn',
     photo: 'https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/gettyimages-3171980.jpg?itok=BroQozLs&resize=1100x1100',
     scores: ['3', '2', '3', '4', '3', '4', '3', '2', '4', '2']
-  }
+  },
+  {
+    name: 'Dean Martin',
+    photo: 'http://www.gstatic.com/tv/thumb/persons/15924/15924_v9_ba.jpg',
+    scores: ['3', '5', '4', '5', '3', '5', '4', '5', '5', '5']
+},
+{
+    name: 'Sylvester Stallone',
+    photo: 'https://dils4you.files.wordpress.com/2015/05/sylvester-stallon-image.jpeg',
+    scores: ['1', '2', '1', '2', '2', '1', '1', '2', '1', '1']
+},
+{
+    name: 'Jane Fonda',
+    photo: 'https://i.dailymail.co.uk/i/pix/2014/12/06/00D1827900000190-2863327-image-m-69_1417869401125.jpg',
+    scores: ['2', '4', '3', '1', '5', '2', '3', '5', '5', '2']
+}
 ];
   
 
 module.exports = friends;
   
 
-let matchMaker = () => {
-  let lastFriend = friends.slice(-1)
-  const calcOne = [];
-  let addition = 0;
-  const calcTwo = [];
-  for (i = 0; i < friends.length - 1; i++) {
-      for (t = 0; t < friends[i].scores.length; t++) {
-          calcOne.push(Math.abs(parseInt(friends[i].scores[t]) - parseInt(lastFriend[0].scores[t])));
-      };
-      for (x = 0; x < calcOne.length; x++) {
-          addition += calcOne[x];
-      };
-      calcTwo.push(addition);
-      calcOne.length = 0;
-      addition = 0;
-  };
-  let minIndex = calcTwo.indexOf(Math.min(...calcTwo));
-  const match = [{ name: friends[minIndex].name, photo: friends[minIndex].photo }];
-  calcTwo.length = 0;
-  return match;
-};
+// let matchMaker = () => {
+//   let lastFriend = friends.slice(-1)
+//   const calcOne = [];
+//   let addition = 0;
+//   const calcTwo = [];
+//   for (i = 0; i < friends.length - 1; i++) {
+//     for (t = 0; t < friends[i].scores.length; t++) {
+//       calcOne.push(Math.abs(parseInt(friends[i].scores[t]) - parseInt(lastFriend[0].scores[t])));
+//     };
+//     for (x = 0; x < calcOne.length; x++) {
+//       addition += calcOne[x];
+//     };
+//     calcTwo.push(addition);
+//     calcOne.length = 0;
+//     addition = 0;
+//   };
+//   let minIndex = calcTwo.indexOf(Math.min(...calcTwo));
+//   const match = [{ name: friends[minIndex].name, photo: friends[minIndex].photo }];
+//   calcTwo.length = 0;
+//   return match;
+// };
 
-module.exports = {
-  friends,
-  matchMaker
-};
+// module.exports = {
+//   friends,
+//   matchMaker
+// };
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,21 +1,42 @@
 
 const friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     console.log('displaying friends data')
     res.json(friends);
   });
 
-  // app.get('/api/friends/match', function(req, res) { 
-  //   res.json(friends.matchMaker()); 
-  // })
+  app.get('/api/friends/match', function (req, res) {
+    console.log('calculating match')
+    let lastFriend = friends.slice(-1)
+    const calcOne = [];
+    let addition = 0;
+    const calcTwo = [];
+    for (i = 0; i < friends.length - 1; i++) {
+        for (t = 0; t < friends[i].scores.length; t++) {
+            calcOne.push(Math.abs(parseInt(friends[i].scores[t]) - parseInt(lastFriend[0].scores[t])));
+        };
+        for (x = 0; x < calcOne.length; x++) {
+            addition += calcOne[x];
+        };
+        calcTwo.push(addition);
+        calcOne.length = 0;
+        addition = 0;
+    };
+    let minIndex = calcTwo.indexOf(Math.min(...calcTwo));
+    const match = [{ name: friends[minIndex].name, photo: friends[minIndex].photo }];
+    calcTwo.length = 0;
+    res.json(match);
+  })
 
-  app.post('api/friends', function(req, res) { 
+  app.post('/api/friends', function (req, res) {
     console.log('adding new friend to friends array')
-    friends.push(req.body); 
+    friends.push(req.body);
     res.json(true);
+    console.log(friends)
   })
 }
- 
+
+
